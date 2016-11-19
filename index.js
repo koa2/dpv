b7c74001bdd84954e884e7c624cc3979d5154888
i5ting <i5ting@126.com>
2016-11-19 17:25:35 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
  * Module dependencies.
  */
 
-/**
- * Prototype.
- */
-
 module.exports = function(fn) {
   const fnText = fn.toString()
   const argDecl = fnText.replace(STRIP_COMMENTS, '').match(FN_ARGS)
@@ -22,15 +18,12 @@ module.exports = function(fn) {
   var result = {}
 
   argDecl[2].split(FN_ARG_SPLIT).forEach(function (arg) {
-    console.log(arg)
-    
     if (/=/.test(arg)) {
       var _re = arg.split('=');
       var k = _re[0]
       var v = eval(_re[1])
       result[k] = v
-    }
-      
+    }  
   })
 
   return result
